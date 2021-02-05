50536b8ad35733f1c424df306fa3f255b2dbadb8
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-02-05 21:18:01 +0100

    Add files via upload

---
+++ Partitioning/run-me.sh
@@ -0,0 +1,6 @@
+# compile and run simulation
+gcc -o3 partitioning-simulation.c -lm -o partitioning-simulation.ce
+
+./partitioning-simulation.ce
+
+# when complete, use gnuplot script for visualisation
