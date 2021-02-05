7185a50b8ba6266fb7a58fdfb035aec3789ba83b
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-02-05 22:45:50 +0100

    Add files via upload

--- run-me.sh
+++ run-me.sh
@@ -1,23 +1,28 @@
+# model fitting to mouse data
 cd Mouse
 chmod +x run-me.sh
 ./run-me.sh &
 cd ..
 
+# gene expression patterns in plant shoot apical meristems
 cd SAM
 chmod +x run-me.sh
 ./run-me.sh &
 cd ..
 
+# presence/absence of oDNA recombination genes across taxa
 cd Phylogenetics
 chmod +x run-me.sh
 ./run-me.sh &
 cd ..
 
+# genetic implications of physical oDNA partitioning at cell divisions
 cd Partitioning
 chmod +x run-me.sh
 ./run-me.sh &
 cd ..
 
+# stochastic simulations testing theory
 cd Stochastics
 chmod +x run-me.sh
 ./run-me.sh &
