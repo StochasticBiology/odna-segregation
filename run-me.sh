231a73690badb06f16c552de368861624a17e4b8
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-02-05 22:44:18 +0100

    Add files via upload

---
+++ run-me.sh
@@ -0,0 +1,24 @@
+cd Mouse
+chmod +x run-me.sh
+./run-me.sh &
+cd ..
+
+cd SAM
+chmod +x run-me.sh
+./run-me.sh &
+cd ..
+
+cd Phylogenetics
+chmod +x run-me.sh
+./run-me.sh &
+cd ..
+
+cd Partitioning
+chmod +x run-me.sh
+./run-me.sh &
+cd ..
+
+cd Stochastics
+chmod +x run-me.sh
+./run-me.sh &
+cd ..
