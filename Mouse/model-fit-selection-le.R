36c81188218f03cc8f9e2ffcb93782f34fe85290
Stochastic Biology Group <50171196+StochasticBiology@users.noreply.github.com>
2021-02-10 07:38:27 +0100

    Update model-fit-selection-le.R

--- Mouse/model-fit-selection-le.R
+++ Mouse/model-fit-selection-le.R
@@ -7,6 +7,8 @@ model.lik <- function(theta, t, Y, model = 0, output=1) {
   rho = theta[3]
   n = exp(theta[4])
 
+  # different model structures. < 3, neutral; >= 3, selection
+  # model % 3 == 0, (i)+(ii)+(v); 1 (ii); 2 (i)+(v)
   if(model < 3) {
     mu = 1/2+(t-t)
     if(model == 1) { v0 = 0 }
@@ -20,6 +22,7 @@ model.lik <- function(theta, t, Y, model = 0, output=1) {
     sigma = sqrt( v0 + (exp(-2/(1+exp(rho*t))) * (4*exp(1)*nuf + 4*exp(1+rho*t)*nuf + exp(2/(1+exp(rho*t)) + rho*t)*(rho - 4*nuf) - exp(2/(1+exp(rho*t)))*(4*nuf+rho))) / (4*(1+exp(rho*t))*n*rho) );
   }
 
+  # if requested, just output model variance or mean
   if(output == 4)
   {
     return(sigma*sigma)
@@ -29,6 +32,7 @@ model.lik <- function(theta, t, Y, model = 0, output=1) {
     return( log((mu*(0.5-1))/(0.5*(mu-1))) )
   }
   
+  # delta method to deal with heteroplasmy transformation
   dist.mean = log(mu/(1-mu)) + (2*mu-1)*sigma*sigma / (2*(mu-1)*(mu-1)*mu*mu)
   dist.var = sigma*sigma / ((mu-1)*(mu-1)*mu*mu)
   dist.sd = sqrt(dist.var)
@@ -78,8 +82,10 @@ theta.3 = opt.model.3$par
 theta.4 = opt.model.4$par
 theta.5 = opt.model.5$par
 
+# compare models with observed V'(h) summary statistics
 vprime.tab = read.table("Data/le-age-vprime.csv")
 
+# model predictions for V'(h)
 preds.0 = model.lik(theta.0, vprime.tab[,1], 0, model=0, output=4)
 preds.1 = model.lik(theta.1, vprime.tab[,1], 0, model=1, output=4)
 preds.2 = model.lik(theta.2, vprime.tab[,1], 0, model=2, output=4)
@@ -95,6 +101,7 @@ preds.3.line = model.lik(theta.3, t.seq, 0, model=3, output=4)
 preds.4.line = model.lik(theta.4, t.seq, 0, model=4, output=4)
 preds.5.line = model.lik(theta.5, t.seq, 0, model=5, output=4)
 
+# compute R2 values
 r2.0 = 1 - sum((preds.0-vprime.tab[,2])**2) / sum((vprime.tab[,2]-mean(vprime.tab[,2]))**2)
 r2.1 = 1 - sum((preds.1-vprime.tab[,2])**2) / sum((vprime.tab[,2]-mean(vprime.tab[,2]))**2)
 r2.2 = 1 - sum((preds.2-vprime.tab[,2])**2) / sum((vprime.tab[,2]-mean(vprime.tab[,2]))**2)
@@ -109,30 +116,16 @@ r2.log.3 = 1 - sum((log(preds.3)-log(vprime.tab[,2]))**2) / sum((log(vprime.tab[
 r2.log.4 = 1 - sum((log(preds.4)-log(vprime.tab[,2]))**2) / sum((log(vprime.tab[,2])-mean(log(vprime.tab[,2])))**2)
 r2.log.5 = 1 - sum((log(preds.5)-log(vprime.tab[,2]))**2) / sum((log(vprime.tab[,2])-mean(log(vprime.tab[,2])))**2)
 
+# sanity check plot
 plot(vprime.tab[,1], log(vprime.tab[,2]))
 points(t.seq, log(preds.3.line), type="l")
 points(t.seq, log(preds.4.line), type="l")
 points(t.seq, log(preds.5.line), type="l")
 #points(t.seq, log(preds.3.line), type="l")
 
+# R2 values for mean
 mean.tab = read.table("Data/le-age-mean.csv")
 preds.mean.0 = model.lik(theta.0, mean.tab[,1], 0, model=0, output=5)
 preds.mean.3 = model.lik(theta.3, mean.tab[,1], 0, model=3, output=5)
 r2.mean.0 = 1 - sum((preds.mean.0-mean.tab[,2])**2) / sum((mean.tab[,2]-mean(mean.tab[,2]))**2)
 r2.mean.3 = 1 - sum((preds.mean.3-mean.tab[,2])**2) / sum((mean.tab[,2]-mean(mean.tab[,2]))**2)
-
-# construct traces of moments with best parameterisations
-#for(t in 0:400) {
-#  mean.0 = model.lik(theta.0, t, 0, model = 0, output = 2)
-#  sd.0 = model.lik(theta.0, t, 0, model = 0, output = 3)
-#  mean.1 = model.lik(theta.1, t, 0, model = 1, output = 2)
-#  sd.1 = model.lik(theta.1, t, 0, model = 1, output = 3)
-#  if(t == 0) {
-#    moments.table = c(t, mean.0, sd.0, mean.1, sd.1)
-#  }
-#  else {
-#    moments.table = rbind(moments.table, c(t, mean.0, sd.0, mean.1, sd.1))
-#  }
-#}
-#write.table(moments.table, file="models-selection.txt", row.names=F, col.names=F)
-
